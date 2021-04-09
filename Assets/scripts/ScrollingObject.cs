b85b1a81c26d7944e1124b6ce0b4568b803a829b
cogjsdl3 <80082555+cogjsdl3@users.noreply.github.com>
2021-04-09 11:42:46 +0900

    0409곽유찬

---
+++ Assets/scripts/ScrollingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+    public float speed = 10f;
+   
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+}
